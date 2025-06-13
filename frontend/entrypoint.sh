aff7c73e6b354f7967ff9be26f23a055b4948e92
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-13 09:50:57 -0400

    Delete frontend/entrypoint.sh

--- frontend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# replace ${…} placeholders in env.js at container start
-envsubst '${SONARR_URL} ${SONARR_API_KEY}' \
-  < /usr/share/nginx/html/env.js \
-  > /usr/share/nginx/html/env.js
-
-# launch nginx
-exec nginx -g 'daemon off;'
