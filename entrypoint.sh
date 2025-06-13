232414be5b66910f5ec4ded6f07157ed75787c37
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-13 10:17:58 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ envsubst '${SONARR_URL} ${SONARR_API_KEY}' \
   > /usr/share/nginx/html/env.js
 
 # kick off your existing scanner in the background
-python watcher.py
+python watcher.py &
 
 # start nginx in foreground
 exec nginx -g 'daemon off;'
