bec3f1dde980b2295dc3046e72b5d4cec0663d9d
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-13 09:52:23 -0400

    Update and rename start.sh to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+# inject real Sonarr vars into the static env.js
+envsubst '${SONARR_URL} ${SONARR_API_KEY}' \
+  < /usr/share/nginx/html/env.js \
+  > /usr/share/nginx/html/env.js
+
+# start nginx in foreground
+exec nginx -g 'daemon off;'
+
+# kick off your existing scanner in the background
+python watcher.py &
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-# kick off your existing scanner in the background
-python watcher.py &
