c2fec98383626e401177cbc296c29adf650f4880
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-14 14:10:28 -0400

    Create Layout.jsx

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import './Layout.css';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand={false} className="mb-3">
+        <Navbar.Brand className="ms-3">Analyzarr</Navbar.Brand>
+      </Navbar>
+
+      <Container fluid>
+        <Row>
+          <Col xs={2} className="sidebar p-0">
+            <Nav className="flex-column bg-dark vh-100">
+              <Nav.Link className="text-light">Dashboard</Nav.Link>
+              <Nav.Link className="text-light">Settings</Nav.Link>
+              {/* add more links here */}
+            </Nav>
+          </Col>
+          <Col xs={10} className="main-content">
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
