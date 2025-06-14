88c4596ae3901a14454d53eeaaf8ad90ef5d7b4d
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-14 15:34:30 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,18 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+import SeriesDetail from './pages/SeriesDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/series/:seriesId" element={<SeriesDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
