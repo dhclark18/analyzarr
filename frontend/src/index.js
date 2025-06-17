8ecaff9a6495cf96bfdf992d2572e090827aeae5
dhclark18 <30811328+dhclark18@users.noreply.github.com>
2025-06-16 21:31:55 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import SeriesDetail from './pages/SeriesDetail';
+import EpisodeDetail from './pages/EpisodeDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
